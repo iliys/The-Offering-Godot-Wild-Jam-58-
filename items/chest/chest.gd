4918dc1bcf9b301ebc012c437808204535bbb9d2
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-06-14 10:23:30 -0400

    Chest auto-opens

--- items/chest/chest.gd
+++ items/chest/chest.gd
@@ -34,3 +34,26 @@ func _unhandled_input(event: InputEvent) -> void:
 					start_rot + TAU / drops.size() * i))
 
 		open = true
+
+
+func _on_interact_zone_body_entered(body: Node2D) -> void:
+	if open:
+		return
+	var player: Player = body
+	if id >= 0:
+		if (not player.keys.has(id)) or player.keys[id] <= 0:
+			return
+		player.modify_key_count(id, -1)
+
+	var interact_zone: Area2D = $InteractZone
+	interact_zone.queue_free()
+	var animated_sprite: AnimatedSprite2D = $AnimatedSprite
+	animated_sprite.play("open")
+	var start_rot := randf_range(-PI, PI)
+	for i in drops.size():
+		var drop: Node2D = drops[i].instantiate()
+		add_sibling(drop)
+		drop.global_position = global_position + (Vector2(drop_distance, 0.0).rotated(
+				start_rot + TAU / drops.size() * i))
+
+	open = true
