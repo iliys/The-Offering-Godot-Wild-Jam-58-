77838a47f6a8031a05e7b1011df11ca86703a0f5
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-06-15 13:01:50 -0400

    Colored gates block light

--- triggers/hit_trigger/colored_switch/colored_switch.gd
+++ triggers/hit_trigger/colored_switch/colored_switch.gd
@@ -11,11 +11,19 @@ extends Node2D
 func _ready() -> void:
 	set_gates(red, red_up)
 	set_gates(blue, not red_up)
+	init_gates(red)
+	init_gates(blue)
 
 
 func set_gates(gates: Node2D, up: bool) -> void:
 	for gate in gates.get_children():
 		gate.active = up
+
+
+func init_gates(gates: Node2D) -> void:
+	for gate in gates.get_children():
+		gate.set_collision_layer_value(8, false)
+		gate.set_collision_layer_value(10, true)
 
 
 func _on_hit_trigger_toggled(_on: bool) -> void:
