d89f18a8fee8a2eee448004b07c326ea3c52be51
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-06-15 14:45:28 -0400

    Created totems

---
+++ map/`totems/totem.gd
@@ -0,0 +1,26 @@
+class_name Totem
+extends StaticBody2D
+
+
+signal activated
+
+@export var id := 3
+
+@onready var sprite: Sprite2D = $Sprite
+@onready var diamond_sprite: Sprite2D = $DiamondSprite
+
+
+func _ready() -> void:
+	sprite.frame = id - 3
+	diamond_sprite.frame = id - 3
+
+
+func _on_interaction_zone_body_entered(body: Node2D) -> void:
+	if (not body.keys.has(id)) or body.keys[id] <= 0:
+		return
+
+	body.modify_key_count(id, -1)
+	var interaction_zone: Area2D = $InteractionZone
+	interaction_zone.queue_free()
+	diamond_sprite.show()
+	activated.emit()
