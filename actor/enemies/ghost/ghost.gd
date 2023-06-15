a42d2157079d7e92b5af318b65ece39e6feff068
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-06-15 02:43:28 -0400

    Ghost petrification fixed, but can spwn on entity

--- actor/enemies/ghost/ghost.gd
+++ actor/enemies/ghost/ghost.gd
@@ -10,7 +10,12 @@ func _go_to_location(location: Vector2, speed_mod := 1.0) -> void:
 
 
 func petrify(location: Vector2) -> void:
-	var petrified_ghost: StaticBody2D = PETRIFIED_GHOST.instantiate()
+	var petrified_ghost: PetrifiedGhost = PETRIFIED_GHOST.instantiate()
+	petrified_ghost.global_position = (location + Vector2.ONE * 8.0).snapped(Vector2.ONE * 16.0) - Vector2.ONE * 8.0
 	call_deferred("add_sibling", petrified_ghost)
-	petrified_ghost.global_position = location
 	queue_free()
+
+
+static func flr(value: float, step: float) -> float:
+	@warning_ignore("integer_division")
+	return float(int(value) / int(step)) * step
---
+++ map/box/petrified_ghost.gd
@@ -0,0 +1,6 @@
+class_name PetrifiedGhost
+extends Box
+
+
+func _ready() -> void:
+	pass # Wil have to do somthing to make the ghost point in the last direction it was facing.
