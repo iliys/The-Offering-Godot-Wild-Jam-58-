bba59e3c735d6c02d38f62994572a3c605109a51
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-06-15 13:28:05 -0400

    Colored gates change img

--- triggers/hit_trigger/colored_switch/colored_switch.gd
+++ triggers/hit_trigger/colored_switch/colored_switch.gd
@@ -3,16 +3,18 @@ extends Node2D
 
 
 @export var red_up := true
+@export var red_img: Texture2D
+@export var yellow_img: Texture2D
 
 @onready var red: Node2D = $Red
-@onready var blue: Node2D = $Blue
+@onready var yellow: Node2D = $Yellow
 
 
 func _ready() -> void:
 	set_gates(red, red_up)
-	set_gates(blue, not red_up)
-	init_gates(red)
-	init_gates(blue)
+	set_gates(yellow, not red_up)
+	init_gates(red, red_img)
+	init_gates(yellow, yellow_img)
 
 
 func set_gates(gates: Node2D, up: bool) -> void:
@@ -20,13 +22,15 @@ func set_gates(gates: Node2D, up: bool) -> void:
 		gate.active = up
 
 
-func init_gates(gates: Node2D) -> void:
+func init_gates(gates: Node2D, img: Texture2D) -> void:
+	gates.modulate = Color.WHITE
 	for gate in gates.get_children():
 		gate.set_collision_layer_value(8, false)
 		gate.set_collision_layer_value(10, true)
+		gate.sprite.texture = img
 
 
 func _on_hit_trigger_toggled(_on: bool) -> void:
 	red_up = not red_up
 	set_gates(red, red_up)
-	set_gates(blue, not red_up)
+	set_gates(yellow, not red_up)
